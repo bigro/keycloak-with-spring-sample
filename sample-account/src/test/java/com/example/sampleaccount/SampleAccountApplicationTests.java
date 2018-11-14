b0bf2839684798dbfa7be407eece9311b2a3fe03
Yuuki Ooguro <kitkat.jaja@gmail.com>
2018-11-14 17:35:01 +0900

    sample-accountプロジェクト作成

---
+++ sample-account/src/test/java/com/example/sampleaccount/SampleAccountApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.sampleaccount;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleAccountApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
