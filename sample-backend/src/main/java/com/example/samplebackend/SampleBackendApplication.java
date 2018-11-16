32c8998d48702739f4f228a16e613ee0aa9e403c
Yuuki Ooguro <kitkat.jaja@gmail.com>
2018-11-16 16:36:29 +0900

    バックエンドのサンプルプロジェクト作成

---
+++ sample-backend/src/main/java/com/example/samplebackend/SampleBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.samplebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleBackendApplication.class, args);
+	}
+}
---
+++ sample-backend/src/test/java/com/example/samplebackend/SampleBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.samplebackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
